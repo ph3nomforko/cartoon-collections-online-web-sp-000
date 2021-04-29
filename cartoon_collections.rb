dc11414cf7e05175000640e8e0e2feb7a57929a3
Codi Kraus <codi.kraus@gmail.com>
2021-04-29 22:00:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,9 @@ end
 
 def summon_captain_planet(array)
   array.collect do |planeteer|
-  planeteer.capitalize
-end
+    planeteer.capitalize
+    planeteer << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
