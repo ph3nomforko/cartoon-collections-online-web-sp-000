dfe2828c80fbd767ec6788b4ea5af1ed4bd836c6
Codi Kraus <codi.kraus@gmail.com>
2021-04-29 22:10:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.detect do |snack|
-    snack == cheese_types
+    cheese_types.include?(snack)
   end
 end
